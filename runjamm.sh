eaf6ed15ffd5fe281120d0f922a4ad137f957258
nattifftoffe <philipp.julia92@gmail.com>
2017-01-17 14:41:58 -0800

    runjamm.sh extension

--- runjamm.sh
+++ runjamm.sh
@@ -1,26 +1,42 @@
-#!/bin/bash
-#$ -l h_vmem=30G
-#$ -m bes
-#$ -pe smp 4
-#$ -cwd
-#$ -e error.txt
-#$ -o out.txt
+# -S /bin/bash
 
-
-mkdir ./output
+# divide bedfiles into plus and minus strand
 
 set -e
 
 JAMM="/campusdata/juphilip/software/jamm/JAMM.sh"
-CHR_LENGTH="/campusdata/juphilip/datasets/human_annotation/chromsize.txt"
-# ANNOTATION="/campusdata/Julia/annotation/mergedannotationX+.bed"
+# ANNOTATION="mergedannotation.bed"
+CHR_LENGTH="/campusdata/juphilip/datasets/human_annotation/chromsize_imp3.txt"
 MODE="normal"
-INPUT="/campusdata/juphilip/datasets/imp3/panc1/bedfiles"
-OUTPUT="/campusdata/juphilip/datasets/imp3/panc1/bedfiles"
-BIN="30"
-# BACKGROUND="/campusdata/Julia/CG6422/bedfiles/ChrX/background/plus/CG6422_INPUT_all.bed"
+INPUT="/campusdata/juphilip/datasets/imp3/panc1/bedfiles/plus"
+OUTPUT="/campusdata/juphilip/results/imp3/JAMM_test/f1_bin50_normal/plus/"
+BIN="50"
+# BACKGROUND="input.bed"
 
-bash $JAMM -s $INPUT -o ./output -g $CHR_LENGTH -b $BIN -m $MODE
+bash $JAMM -s $INPUT -o $OUTPUT -g $CHR_LENGTH -b $BIN -m $MODE -f 1,1,1
+
+
+set -e
+
+JAMM="/campusdata/juphilip/software/jamm/JAMM.sh"
+CHR_LENGTH="/campusdata/juphilip/datasets/human_annotation/chromsize_imp3.txt"
+MODE="normal"
+INPUT="/campusdata/juphilip/datasets/imp3/panc1/bedfiles/minus"
+OUTPUT="/campusdata/juphilip/results/imp3/JAMM_test/f1_bin50_normal/minus/"
+BIN="50"
+
+bash $JAMM -s $INPUT -o $OUTPUT -g $CHR_LENGTH -b $BIN -m $MODE -f 1,1,1
+
+cd /campusdata/juphilip/results/imp3/JAMM_test/f1_bin50_normal/plus/peaks
+less filtered.peaks.narrowPeak | awk -F"\t" 'BEGIN{OFS=FS}{print $1,$2,$3,$4,$7,"+",$5,$8,$9,$10}' > filtered_peaks_plus.bed
+
+cd /campusdata/juphilip/results/imp3/JAMM_test/f1_bin50_normal/minus/peaks
+less filtered.peaks.narrowPeak | awk -F"\t" 'BEGIN{OFS=FS}{print $1,$2,$3,$4,$7,"-",$5,$8,$9,$10}' > filtered_peaks_minus.bed
+
+cd /campusdata/juphilip/results/imp3/JAMM_test/f1_bin50_normal
+cat plus/peaks/filtered_peaks_plus.bed minus/peaks/filtered_peaks_minus.bed > filtered_peaks_all.bed
+
 
 exit 0
+
 
